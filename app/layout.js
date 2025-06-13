5c26b7bf622657bb4909cb47b92df6991ed1ceee
Jeremy Voegele <voegelejj@gmail.com>
2025-06-12 19:33:32 -0700

    fixed global layout

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,17 @@
 import './global.css';
-import '../public/assets/bootstrap.min.css';
-import Head from 'next/head';
+import NavBar from './components/NavBar';
 
 export const metadata = {
   title: 'Jeremy Voegele Portfolio',
   description: 'IT Specialist | Developer | Veteran',
-  icons: {
-    icon: '/images/default_avatar.png',
-  },
+  icons: { icon: '/images/default_avatar.png' },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body>
+        <NavBar />
         {children}
       </body>
     </html>
